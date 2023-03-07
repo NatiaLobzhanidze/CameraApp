c40d68a8d65434fcf7508bf70b721cc95f2a3a8b
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2023-03-07 13:47:49 +0400

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Natia's Mac on 07.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
